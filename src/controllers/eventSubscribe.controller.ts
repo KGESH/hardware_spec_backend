e0f83bcb184e8da2a86f7998f92b63c27327a724
J222 <baram789456@gmail.com>
2024-01-28 01:48:18 +0100

    [Refactor] Change dir path

--- src/controllers/eventSubscribe.controller.ts
+++ src/controllers/eventSubscribe.controller.ts
@@ -1,22 +1,95 @@
 import { Controller, Logger } from '@nestjs/common';
 import { EventPattern, Payload, Transport } from '@nestjs/microservices';
 import { EstimateRequestDto } from '../dtos/estimate/estimate.dto';
-import { EstimateAIService } from '../services/estimateAI.service';
+import { EstimateAIService } from '../services/estimate/estimateAI.service';
 import { ESTIMATE_CREATE_EVENT } from '../constants/estimate.constant';
+import { ComputerService } from '../services/computer/computer.service';
+import { ShopService } from '../services/shop/shop.service';
+import { EntityNotfoundException } from '../exceptions/entityNotfound.exception';
+import { getCurrency } from '../utils/currency';
+import { EstimateService } from '../services/estimate/estimate.service';
+import { AIEstimateResponseDto } from '../dtos/estimate/ai.dto';
 
 @Controller()
 export class EventSubscribeController {
   private readonly logger = new Logger(EventSubscribeController.name);
 
-  constructor(private readonly estimateAIService: EstimateAIService) {}
+  constructor(
+    private readonly computerService: ComputerService,
+    private readonly estimateAIService: EstimateAIService,
+    private readonly shopService: ShopService,
+    private readonly estimateService: EstimateService,
+  ) {}
 
   @EventPattern(ESTIMATE_CREATE_EVENT, Transport.REDIS)
   async requestEstimate(@Payload() data: EstimateRequestDto): Promise<any> {
     this.logger.debug('EventPattern EXTERNAL API REQUEST START', data);
 
-    const estimate = await this.estimateAIService.requestEstimate(data);
+    const computer = await this.computerService.getComputer(data.computer);
 
-    this.logger.debug('EventPattern EXTERNAL API REQUEST DONE', estimate);
+    const shop = await this.shopService.findBy({
+      id: data.shopId,
+    });
+
+    if (!shop) {
+      throw new EntityNotfoundException({
+        message: `Shop not found`,
+      });
+    }
+
+    // Cache estimate create status
+    await this.estimateAIService.cacheEstimateStatus(
+      {
+        estimateId: data.estimateId,
+      },
+      {
+        status: 'pending',
+        shopId: data.shopId,
+        encodedId: data.encodedId,
+        estimateId: data.estimateId,
+      },
+    );
+
+    const estimate = await this.estimateService.createEstimate({
+      id: data.estimateId,
+      name: 'Estimate',
+      country: shop.country,
+    });
+
+    // Todo: make dto
+    const estimateParts = await this.estimateAIService.requestEstimate({
+      computer,
+      currency: getCurrency(shop.country),
+      shopId: shop.id,
+      estimateId: estimate.id,
+    });
+
+    const response: AIEstimateResponseDto =
+      estimateParts.length === 0
+        ? {
+            status: 'error',
+            message: 'Estimate not created',
+            shopId: shop.id,
+            estimateId: estimate.id,
+            encodedId: data.encodedId,
+          }
+        : {
+            status: 'success',
+            shopId: shop.id,
+            estimateId: estimate.id,
+            encodedId: data.encodedId,
+            estimates: estimateParts,
+          };
+
+    // Cache created estimate
+    await this.estimateAIService.cacheEstimateStatus(
+      {
+        estimateId: data.estimateId,
+      },
+      response,
+    );
+
+    this.logger.debug('EventPattern EXTERNAL API REQUEST DONE', response);
 
     return { message: 'DONE' };
   }
---
+++ src/modules/infra/eventSubscribe.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { EventSubscribeController } from '../../controllers/eventSubscribe.controller';
+import { EstimateModule } from '../estimate/estimate.module';
+import { EstimateAIService } from '../../services/estimate/estimateAI.service';
+import { RedisModule } from './redis.module';
+import { ShopModule } from '../shop/shop.module';
+
+@Module({
+  imports: [RedisModule, EstimateModule, ShopModule],
+  controllers: [EventSubscribeController],
+  providers: [EstimateAIService],
+})
+export class EventSubscribeModule {}
