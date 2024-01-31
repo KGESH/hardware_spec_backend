248e1af6c01c4a61b13758071415eec4f6969eb1
J222 <baram789456@gmail.com>
2024-02-01 00:50:41 +0100

    [Refactor] Change prisma data source url

--- src/services/infra/prisma.service.ts
+++ src/services/infra/prisma.service.ts
@@ -1,9 +1,15 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { ConfigsService } from '../../configs/configs.service';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger(PrismaService.name);
+
+  constructor(private readonly configService: ConfigsService) {
+    super({ datasourceUrl: configService.env.DATABASE_URL });
+  }
+
   async onModuleInit() {
     await this.$connect();
     this.logger.log('Prisma connected');
