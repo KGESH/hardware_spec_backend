1ebd03d8e6c397580358fda5fada880e4f61478b
J222 <baram789456@gmail.com>
2024-02-12 22:32:59 +0100

    [Feat] Add test db cleanup

--- src/services/infra/prisma.service.ts
+++ src/services/infra/prisma.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { ConfigsService } from '../../configs/configs.service';
+import { isProduction } from '../../utils/production.util';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
@@ -13,5 +14,39 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
     this.logger.log('Prisma connected');
+  }
+
+  async cleanDatabase() {
+    if (isProduction()) return;
+
+    const tablenames = await this.$queryRaw<
+      Array<{ tablename: string }>
+    >`SELECT tablename FROM pg_tables WHERE schemaname='public'`;
+
+    const tables = tablenames
+      .map(({ tablename }) => tablename)
+      .filter((name) => name !== '_prisma_migrations')
+      .map((name) => `"public"."${name}"`)
+      .join(', ');
+
+    try {
+      await this.$executeRawUnsafe(`TRUNCATE TABLE ${tables} CASCADE;`);
+    } catch (error) {
+      console.log({ error });
+    }
+    //
+    // const models = Object.keys(this).filter((key) => key[0] !== '_');
+    //
+    // return Promise.all(
+    //   models.map((tableName) => {
+    //     return this[tableName].deleteMany();
+    //   }),
+    // );
+    // // const models = Reflect.ownKeys(this).filter((key) => key[0] !== '_');
+    // // models.forEach((model) =>
+    // //   this.logger.debug(`cleanDatabase: ${model.toString()}`),
+    // // );
+    // //
+    // // return Promise.all(models.map((modelKey) => this[modelKey].deleteMany()));
   }
 }
